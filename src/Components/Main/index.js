d2ebfecec21adb6c05b1ac5f179414e324c723f3
Mitchell Armstrong <mra0211@gmail.com>
2023-05-11 17:47:40 -0400

    Changed the background of the main component to be a video

--- src/Components/Main/index.js
+++ src/Components/Main/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import Video from '../../assets/videos/bg-test.mp4';
 
 function Main() {
   return (
     <section id="Main" className="text-center flex justify-center">
+      <video autoPlay loop muted>
+        <source src={Video} type="video/mp4" />
+      </video>
       <div className="flex flex-col main-div">
         <header className="main-header">Hello, I'm Mitchell!</header>
         <p className="main-description">
