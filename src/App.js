07b181593d17a3a8b44cf6fbb07d594bc3f75541
Mitchell Armstrong <mra0211@gmail.com>
2023-05-15 20:15:45 -0400

    Finished styling the Other Projects section and added a button to return users to the top of the page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Main from './Components/Main';
 import Nav from './Components/Nav';
 import About from './Components/About';
 import Projects from './Components/Projects';
 import OtherProjects from './Components/OtherProjects';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Contact from './Components/Contact';
+import arrow from './assets/other/up-arrow.svg';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
+  const [showTopButton, setShowTopButton] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const aboutComponent = document.getElementById('About');
+      if (aboutComponent && window.scrollY >= aboutComponent.offsetTop) {
+        setShowTopButton(true);
+      } else {
+        setShowTopButton(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
     <main>
       <Router>
@@ -16,6 +36,15 @@ function App() {
         <About />
         <Projects />
         <OtherProjects />
+        <Contact />
+        {showTopButton && (
+          <a href="#top" class="t-top">
+            <span class="up-arrow">
+              <img src={arrow} alt="up arrow icon" />
+            </span>
+            Top
+          </a>
+        )}
       </Router>
     </main>
   );
--- src/Components/OtherProjects/index.js
+++ src/Components/OtherProjects/index.js
@@ -9,7 +9,7 @@ function OtherProjects() {
       <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-12">
         <a href="https://hidden-bastion-66681.herokuapp.com/" target="blank">
           <li className="other-project flex flex-col">
-            <div className="flex justify-between mb-4">
+            <div className="flex justify-between mb-4 other-project-top">
               <h1 className="other-project-type pt-1">SOLO</h1>
               <ul className="flex">
                 <div className="mx-2 other-link-icon" title="GitHub Repo">
@@ -39,8 +39,8 @@ function OtherProjects() {
               you can also click the install button to have it on your computer
               to have access to it anytime you want.
             </p>
-            <ul className="other-project-tech-list flex pt-8">
-              <li className="other-project-tech mr-2">HTML</li>
+            <ul className="other-project-tech-list pt-8">
+              <li className="other-project-tech mx-2">HTML</li>
               <li className="other-project-tech mx-2">CSS</li>
               <li className="other-project-tech mx-2">JavaScript</li>
               <li className="other-project-tech mx-2">ExpressJS</li>
@@ -48,11 +48,205 @@ function OtherProjects() {
             </ul>
           </li>
         </a>
-        <li className="other-project"></li>
-        <li className="other-project"></li>
-        <li className="other-project"></li>
-        <li className="other-project"></li>
-        <li className="other-project"></li>
+
+        <a href="https://scarelite.github.io/run-buddy/" target="blank">
+          <li className="other-project flex flex-col">
+            <div className="flex justify-between mb-4 other-project-top">
+              <h1 className="other-project-type pt-1">SOLO</h1>
+              <ul className="flex">
+                <div className="mx-2 other-link-icon" title="GitHub Repo">
+                  <a
+                    href="https://github.com/ScarElite/run-buddy"
+                    target="blank"
+                  >
+                    <i class="fa-brands fa-github"></i>
+                  </a>
+                </div>
+                <div className="mx-2 other-link-icon" title="Website">
+                  <a
+                    href="https://scarelite.github.io/run-buddy/"
+                    target="blank"
+                  >
+                    <i class="fa-solid fa-up-right-from-square"></i>
+                  </a>
+                </div>
+              </ul>
+            </div>
+            <h1 className="other-project-title mb-4">Run Buddy</h1>
+            <p className="other-project-description">
+              Run Buddy is a website that offers fitness training services. It
+              is built purely with HTML and CSS only. This was one of the first
+              websites I created in the UNC Coding Boot Camp.
+            </p>
+            <ul className="other-project-tech-list pt-8">
+              <li className="other-project-tech mx-2">HTML</li>
+              <li className="other-project-tech mx-2">CSS</li>
+            </ul>
+          </li>
+        </a>
+
+        <a href="https://guarded-beach-97718.herokuapp.com/" target="blank">
+          <li className="other-project flex flex-col">
+            <div className="flex justify-between mb-4 other-project-top">
+              <h1 className="other-project-type pt-1">SOLO</h1>
+              <ul className="flex">
+                <div className="mx-2 other-link-icon" title="GitHub Repo">
+                  <a
+                    href="https://github.com/ScarElite/just-tech-news"
+                    target="blank"
+                  >
+                    <i class="fa-brands fa-github"></i>
+                  </a>
+                </div>
+                <div className="mx-2 other-link-icon" title="Website">
+                  <a
+                    href="https://guarded-beach-97718.herokuapp.com/"
+                    target="blank"
+                  >
+                    <i class="fa-solid fa-up-right-from-square"></i>
+                  </a>
+                </div>
+              </ul>
+            </div>
+            <h1 className="other-project-title mb-4">Just Tech News</h1>
+            <p className="other-project-description">
+              Just Tech News is a website that allows people to create posts
+              about things they find interesting. Users can sign up and blog
+              about things they find interesting. Other people can see their
+              posts as well as comment on them.
+            </p>
+            <ul className="other-project-tech-list pt-8">
+              <li className="other-project-tech mx-2">HTML</li>
+              <li className="other-project-tech mx-2">CSS</li>
+              <li className="other-project-tech mx-2">JavaScript</li>
+              <li className="other-project-tech mx-2">HandlebarsJS</li>
+              <li className="other-project-tech mx-2">ExpressJS</li>
+              <li className="other-project-tech mx-2">MySQL</li>
+              <li className="other-project-tech mx-2">Heroku</li>
+            </ul>
+          </li>
+        </a>
+
+        <a href="https://lit-garden-80441.herokuapp.com/notes" target="blank">
+          <li className="other-project flex flex-col">
+            <div className="flex justify-between mb-4 other-project-top">
+              <h1 className="other-project-type pt-1">SOLO</h1>
+              <ul className="flex">
+                <div className="mx-2 other-link-icon" title="GitHub Repo">
+                  <a
+                    href="https://github.com/ScarElite/Note-Taker"
+                    target="blank"
+                  >
+                    <i class="fa-brands fa-github"></i>
+                  </a>
+                </div>
+                <div className="mx-2 other-link-icon" title="Website">
+                  <a
+                    href="https://lit-garden-80441.herokuapp.com/notes"
+                    target="blank"
+                  >
+                    <i class="fa-solid fa-up-right-from-square"></i>
+                  </a>
+                </div>
+              </ul>
+            </div>
+            <h1 className="other-project-title mb-4">Note Taker</h1>
+            <p className="other-project-description">
+              This application was created to provide you with a way to write,
+              save, and delete notes through a server. Simply click the "Get
+              Started" button and it will take you to a new page that displays
+              the notes you already have saved and gives you the ability to
+              create/delete a note.
+            </p>
+            <ul className="other-project-tech-list pt-8">
+              <li className="other-project-tech mx-2">HTML</li>
+              <li className="other-project-tech mx-2">CSS</li>
+              <li className="other-project-tech mx-2">JavaScript</li>
+              <li className="other-project-tech mx-2">jQuery</li>
+              <li className="other-project-tech mx-2">NodeJS</li>
+              <li className="other-project-tech mx-2">ExpressJS</li>
+              <li className="other-project-tech mx-2">Heroku</li>
+            </ul>
+          </li>
+        </a>
+
+        <a href="https://warm-atoll-72156.herokuapp.com/" target="blank">
+          <li className="other-project flex flex-col">
+            <div className="flex justify-between mb-4 other-project-top">
+              <h1 className="other-project-type pt-1">SOLO</h1>
+              <ul className="flex">
+                <div className="mx-2 other-link-icon" title="GitHub Repo">
+                  <a
+                    href="https://github.com/ScarElite/deep-thoughts"
+                    target="blank"
+                  >
+                    <i class="fa-brands fa-github"></i>
+                  </a>
+                </div>
+                <div className="mx-2 other-link-icon" title="Website">
+                  <a
+                    href="https://warm-atoll-72156.herokuapp.com/"
+                    target="blank"
+                  >
+                    <i class="fa-solid fa-up-right-from-square"></i>
+                  </a>
+                </div>
+              </ul>
+            </div>
+            <h1 className="other-project-title mb-4">Deep Thoughts</h1>
+            <p className="other-project-description">
+              Deep Thoughts is a basic social media app. Simply signup and you
+              can begin creating posts and adding other users as friends. Posts
+              are interactive and can be liked and commented on.
+            </p>
+            <ul className="other-project-tech-list pt-8">
+              <li className="other-project-tech mx-2">HTML</li>
+              <li className="other-project-tech mx-2">CSS</li>
+              <li className="other-project-tech mx-2">JavaScript</li>
+              <li className="other-project-tech mx-2">MongoDB</li>
+              <li className="other-project-tech mx-2">Heroku</li>
+            </ul>
+          </li>
+        </a>
+
+        <a href="https://scarelite.github.io/coding-quiz/" target="blank">
+          <li className="other-project flex flex-col">
+            <div className="flex justify-between mb-4 other-project-top">
+              <h1 className="other-project-type pt-1">SOLO</h1>
+              <ul className="flex">
+                <div className="mx-2 other-link-icon" title="GitHub Repo">
+                  <a
+                    href="https://github.com/ScarElite/coding-quiz"
+                    target="blank"
+                  >
+                    <i class="fa-brands fa-github"></i>
+                  </a>
+                </div>
+                <div className="mx-2 other-link-icon" title="Website">
+                  <a
+                    href="https://scarelite.github.io/coding-quiz/"
+                    target="blank"
+                  >
+                    <i class="fa-solid fa-up-right-from-square"></i>
+                  </a>
+                </div>
+              </ul>
+            </div>
+            <h1 className="other-project-title mb-4">Coding Quiz</h1>
+            <p className="other-project-description">
+              This website was created to provide you with a JavaScript quiz to
+              test your knowledge about things related to JavaScript. The quiz
+              has 10 questions that will appear in random order each time the
+              quiz is started. Getting a question wrong will reduce your time
+              and score.
+            </p>
+            <ul className="other-project-tech-list pt-8">
+              <li className="other-project-tech mx-2">HTML</li>
+              <li className="other-project-tech mx-2">CSS</li>
+              <li className="other-project-tech mx-2">JavaScript</li>
+            </ul>
+          </li>
+        </a>
       </ul>
     </section>
   );
